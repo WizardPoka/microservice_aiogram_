d38dc057b8410920198969ef578fdccd019a158f
WizarPoka <Vasya-nve12401@yandex.ru>
2024-04-19 13:27:57 +0300

    FMS Context сделана мини регистрация по команде /reg

--- app/handlers.py
+++ app/handlers.py
@@ -3,10 +3,16 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
+
+class Reg( StatesGroup):
+    name = State()
+    number = State()
 
 # ====================================================================================
  
@@ -47,3 +53,32 @@ async def get_photo(message: Message):
 async def catalog(callback: CallbackQuery):
     await callback.answer("")
     await callback.message.edit_text("Привет", reply_markup=await kb.inline_cars())
+
+# ====================================================================================
+
+@router.message(Command("reg"))
+async def reg_one(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer("Введите ваше имя")
+
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer("Введите номер телефона")
+
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    print(f'Получено сообщение: {data["name"]}\nНомер: {data["number"]}')
+    await message.answer(f'Регистрация завершена.\nИмя: {data["name"]}\nНомер: {data["number"]}')
+    await state.clear()
+
+# ====================================================================================
+
+@router.message()
+async def log_all_messages(message: Message):
+    print(f"Получено сообщение: {message.text}")
+
+# ====================================================================================
