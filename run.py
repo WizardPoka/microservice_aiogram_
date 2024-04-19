bbb173d5e2f75949a4cd254d14c936e108486de8
WizarPoka <Vasya-nve12401@yandex.ru>
2024-04-19 10:25:18 +0300

    filters Comand, F; получать данные из from_user

--- run.py
+++ run.py
@@ -1,20 +1,47 @@
+# ====================================================================================
+
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import TOKEN
 
+# ====================================================================================
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# ====================================================================================
 
-@dp.message(CommandStart)
+@dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Hello!")
+    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
+
+# ====================================================================================
+
+@dp.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("Команда /help")
+
+@dp.message(F.text == "Как дела?")
+async def how_are_you(message: Message):
+    await message.answer("Хорошо")
+
+# ====================================================================================
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f"ID фото: {message.photo[-1].file_id}")
+
+@dp.message(Command("get_photo"))
+async def get_photo(message: Message):
+    await message.answer_photo(photo="AgACAgIAAxkBAAMXZiIaO1xC0Xef6sX3lNcIaVKnX3gAAtjWMRuFDQFJQbsuuSjYreQBAAMCAAN5AAM0BA",
+                               caption="Лого")
+
+# ====================================================================================
 
 async def main():
     await dp.start_polling(bot)
@@ -26,3 +53,5 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Exit")
+
+# ====================================================================================        
